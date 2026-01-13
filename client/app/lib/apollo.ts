fd321db6dae8a85bfeaf5f00632934dfb80461d5
matthew.millard <matthew.millard@fullscript.com>
2026-01-12 22:45:02 -0500

    Add React Router v7 frontend client with Apollo GraphQL

---
+++ client/app/lib/apollo.ts
@@ -0,0 +1,7 @@
+import { HttpLink } from '@apollo/client'
+import {ApolloClient, InMemoryCache} from '@apollo/client'
+
+export const client = new ApolloClient({
+    link: new HttpLink({uri: "http://localhost:3001/graphql"}),
+    cache: new InMemoryCache()
+})
