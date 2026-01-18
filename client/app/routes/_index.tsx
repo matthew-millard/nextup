f4457c59ed3b7e38b40b65f0e2b409a804c5d2a4
matthew.millard <matthew.millard@fullscript.com>
2026-01-18 14:21:21 -0500

    add ESLint configured to work alongside Prettier

--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 import type { Route } from "./+types/_index";
 import { useQuery } from "@apollo/client/react";
 
-export function meta({ }: Route.MetaArgs) {
+export function meta(_args: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
